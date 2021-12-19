35619ef9fce0adf4576f5ee0c1ac86e36b0587fa
hungnd94 <hungnd307@gmail.com>
2021-12-20 01:56:59 +0700

    Finishing homework 1

--- ss1/hw2.js
+++ ss1/hw2.js
@@ -1,5 +1,25 @@
-let a = document.getElementById("firstNum");
-let b = document.getElementById("secondNum");
+let a = parseInt(prompt('Enter the first number: '));
+let b = parseInt(prompt('Enter the second number: '));
 
-console.log(a);
-console.log(b);
+function checkPrime(k) {
+    let isPrime = true;
+    if (k < 2) {
+        isPrime = false;
+    } else if (k > 3) {
+        let threshold = Math.floor(Math.sqrt(k)) + 1;
+        for (let i = 2; i < threshold; i++) {
+            if (k % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return isPrime;
+}
+
+document.writeln("Here is the list of primes between " + a + " and " + b + ": ");
+for (let num = a; num < b + 1; num++) {
+    if (checkPrime(num)) {
+        document.writeln(num);
+    }
+}
