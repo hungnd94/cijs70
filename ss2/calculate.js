3e81bbc418dc9f5f3168c539ea5c184ef0f8182c
hungnd94 <hungnd307@gmail.com>
2021-12-20 22:47:23 +0700

    Second lesson

---
+++ ss2/calculate.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    return a + b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function factorial(n) {
+    if (n == 1) {
+        return 1;
+    } else {
+        return factorial(n - 1);
+    }
+}
+
+function subtract(a, b) {
+    return a - b;
+}
---
+++ ss2/import.js
@@ -0,0 +1,8 @@
+import {
+    add,
+    subtract,
+    multiply,
+    factorial
+} from './calculate.js';
+
+console.log(add(10, 5));
